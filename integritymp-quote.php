86e87594c2a31b25ec0b8743e760220ea8139a68
Mahmud <me@imahmud.com>
2024-10-28 10:42:26 +0600

    plugin initialize

---
+++ integritymp-quote.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Plugin Name:       IntegrityMP Quoting and Sales Add-On
+ * Plugin URI:        https://integritymp.com/
+ * Description:       A quoting and sales add-on for IntegrityMP, allowing registered customers to request quotes, view past orders, and easily convert quotes into sales. Integrates with WooCommerce for quote management and product catalog access.
+ * Version:           1.0
+ * Requires at least: 6.5
+ * Requires PHP:      7.4
+ * Author:            Mahmudul Hasan
+ * Author URI:        https://imahmud.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/H-Mahmud/integritymp-quote
+ * Text Domain:       integritymp-quote
+ * Domain Path:       /languages
+ * Requires Plugins:  woocommerce
+ */
+
+defined('ABSPATH') || exit;
